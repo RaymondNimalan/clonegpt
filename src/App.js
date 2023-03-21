a430ceef7f1c84aae9ede5b2290e27e3bb2f88d9
Ray Nimalan <raymondnimalan@gmail.com>
2023-03-21 15:50:40 -0400

    feat: created chat log

--- src/App.js
+++ src/App.js
@@ -10,7 +10,22 @@ function App() {
           New Chat
         </div>
       </aside>
-      <section className='chatbox'></section>
+      <section className='chatbox'>
+        <div className='chat-log'>
+          <div className='chat-message'>
+            <div className='avatar'>Me</div>
+            <div className='message'>Hello World</div>
+          </div>
+        </div>
+        <h1 className='title'>CloneGPT</h1>
+        <div className='chat-input-holder'>
+          <textarea rows='1' className='chat-input-textarea'></textarea>
+        </div>
+        <div className='footer'>
+          CloneGPT created by Raymond Nimalan using OpenAI API. Check out my
+          github and portfolio website!
+        </div>
+      </section>
     </div>
   );
 }
