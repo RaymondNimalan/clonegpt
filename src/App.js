887e88596d175ca3c2d9a86018a24b3d9c22d5bc
Ray Nimalan <raymondnimalan@gmail.com>
2023-03-21 14:22:39 -0400

    feat: creating sidebar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <aside className='sidemenu'>
+        <h1>Aside</h1>
+      </aside>
+      <section>'</section>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
